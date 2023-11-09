1445b500a19a838763ed2f3d5c342f6d2f93855d
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2023-11-09 11:02:02 +0100

    ghf

--- VinterProjekt/Program.cs
+++ VinterProjekt/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+using System.Numerics;
+
+Raylib.InitWindow(1700, 900, "Pelican");
+Raylib.SetTargetFPS(60);
+
+Vector2 movement = new Vector2(7, 7);
+
+Texture2D characterImage = Raylib.LoadTexture("gabba.png");
+Rectangle characterRect = new Rectangle(100, 100, 128, 128);
+
+while (!Raylib.WindowShouldClose())
+{
+
+
+
+
+
+
+
+
+
+    Raylib.EndDrawing();
+
+}
