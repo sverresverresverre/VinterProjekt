5bb6fc7216c3c69fc492785fe4fa516def1b634a
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2023-11-16 09:51:49 +0100

    ujuju

--- VinterProjekt/Program.cs
+++ VinterProjekt/Program.cs
@@ -6,11 +6,25 @@ Raylib.SetTargetFPS(60);
 
 Vector2 movement = new Vector2(7, 7);
 
-Texture2D characterImage = Raylib.LoadTexture("gabba.png");
-Rectangle characterRect = new Rectangle(100, 100, 128, 128);
+Texture2D characterImage = Raylib.LoadTexture("peliplayer.png");
+Rectangle characterRect = new Rectangle(200, 300, 128, 128);
+
+Texture2D testenemy = Raylib.LoadTexture("enemy.png");
+
+characterRect.X = 200;
+characterRect.Y = 300;
 
 while (!Raylib.WindowShouldClose())
 {
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.SKYBLUE);
+
+    Raylib.DrawRectangleRec(characterRect, Color.BLANK);
+
+    Raylib.DrawTexture(characterImage, (int) characterRect.X, (int) characterRect.Y, Color.WHITE);
+
+    Raylib.DrawTexture(testenemy, 1000, 300, Color.WHITE);
 
 
 
