2c59346a53373a8bc7e5aaa1796fb464f6740281
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2023-11-21 11:07:18 +0100

    kingking

--- VinterProjekt/Program.cs
+++ VinterProjekt/Program.cs
@@ -1,18 +1,38 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 
+
 Raylib.InitWindow(1700, 900, "Pelican");
 Raylib.SetTargetFPS(60);
 
-Vector2 movement = new Vector2(7, 7);
-
 Texture2D characterImage = Raylib.LoadTexture("peliplayer.png");
+Texture2D characterImageDash = Raylib.LoadTexture("peliplayerdash.png");
 Rectangle characterRect = new Rectangle(200, 300, 128, 128);
 
 Texture2D testenemy = Raylib.LoadTexture("enemy.png");
 
 characterRect.X = 200;
 characterRect.Y = 300;
+
+void Jump()
+{
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE))
+    {
+        characterRect.Y -= 8;
+        Raylib.DrawTexture(characterImage, (int) characterRect.X, (int) characterRect.Y, Color.WHITE);
+    }
+    else if(Raylib.IsKeyDown(KeyboardKey.KEY_W))
+    {
+        characterRect.Y += 0;
+        Raylib.DrawTexture(characterImageDash, (int) characterRect.X, (int) characterRect.Y, Color.WHITE);
+    }
+    else
+    {
+        characterRect.Y += 8;
+        Raylib.DrawTexture(characterImage, (int) characterRect.X, (int) characterRect.Y, Color.WHITE);
+    }
+}
+
 
 while (!Raylib.WindowShouldClose())
 {
@@ -22,9 +42,9 @@ while (!Raylib.WindowShouldClose())
 
     Raylib.DrawRectangleRec(characterRect, Color.BLANK);
 
-    Raylib.DrawTexture(characterImage, (int) characterRect.X, (int) characterRect.Y, Color.WHITE);
-
     Raylib.DrawTexture(testenemy, 1000, 300, Color.WHITE);
+
+    Jump();
 
 
 
